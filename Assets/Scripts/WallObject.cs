9585e35959d5d764d955cc68dffb5d2a70d0f45c
Job van Ellen <job.vanellen@tmc.nl>
2025-01-15 12:46:35 +0100

    broken tile at 1 HP

--- Assets/Scripts/WallObject.cs
+++ Assets/Scripts/WallObject.cs
@@ -4,8 +4,10 @@ using UnityEngine.Tilemaps;
 public class WallObject : CellObject
 {
     public Tile ObstacleTile;
+    public Tile BrokenTile;
     public int Max_HP = 3;
-    private int HP;
+
+    private int m_HP;
 
     private Tile m_OriginalTile;
 
@@ -13,15 +15,19 @@ public class WallObject : CellObject
     public override void Init(Vector2Int cell)
     {
         base.Init(cell);
-        HP = Max_HP;
+        m_HP = Max_HP;
         m_OriginalTile = GameManager.Instance.BoardManager.GetCellTile(cell);
         GameManager.Instance.BoardManager.SetCellTile(cell, ObstacleTile);
     }
 
     public override bool PlayerWantsToEnter()
     {
-        HP--;
-        if(HP <= 0)
+        m_HP--;
+        if(m_HP == 1)
+        {
+            GameManager.Instance.BoardManager.SetCellTile(m_Cell, BrokenTile);
+        }
+        if(m_HP <= 0)
         {
             GameManager.Instance.BoardManager.SetCellTile(m_Cell, m_OriginalTile);
             Destroy(gameObject);
